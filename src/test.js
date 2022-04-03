c994fdb7ed61d80bf8e3fc55f229f92bfd4cb58c
rafiq <rafiq.i@live.co.uk>
2022-04-04 00:10:58 +0600

    comment

--- src/test.js
+++ src/test.js
@@ -1,12 +1,12 @@
-pipeline{
-    agent any
-    tools {nodejs "node"}
-    stages {
-     stage('Build'){
-         steps {
-          git 'https://github.com/linuxrafiq/react-node-jenkins-simple-app.git'
-          sh 'npm install'
-         }
-     }
-    }
-    }
+// pipeline{
+//     agent any
+//     tools {nodejs "node"}
+//     stages {
+//      stage('Build'){
+//          steps {
+//           git 'https://github.com/linuxrafiq/react-node-jenkins-simple-app.git'
+//           sh 'npm install'
+//          }
+//      }
+//     }
+//     }
