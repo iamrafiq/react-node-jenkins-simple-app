ecce2eea2f1fe1ff6276b004497f21ba7027b86f
rafiq <rafiq.i@live.co.uk>
2022-04-03 23:27:23 +0600

    pipeline

---
+++ src/test.js
@@ -0,0 +1,12 @@
+pipeline{
+    agent any
+    tools {nodejs "node"}
+    stages {
+     stage('Build'){
+         steps {
+          git 'https://github.com/linuxrafiq/react-node-jenkins-simple-app.git'
+          sh 'npm install'
+         }
+     }
+    }
+    }
